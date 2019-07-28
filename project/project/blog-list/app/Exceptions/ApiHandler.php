1d90d670d90200677093dca8bc8d0f3535119253
cmming <2942257072@qq.com>
2019-07-29 00:12:32 +0800

    添加自定义错误消息类

--- project/project/blog-list/app/Exceptions/ApiHandler.php
+++ project/project/blog-list/app/Exceptions/ApiHandler.php
@@ -12,28 +12,37 @@ namespace App\Exceptions;
 use Exception;
 use Dingo\Api\Exception\Handler as DingoHandler;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use App\Exceptions\ErrorMessage;
 
 class ApiHandler extends DingoHandler
 {
     public function handle(Exception $exception)
     {
 //        自定义一要捕获的异常
-//        if ($exception instanceof UnauthorizedHttpException) {
-//            $preException = $exception->getPrevious();
-//            if ($preException instanceof
-//                \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
-//                return response()->json(['error' => 'TOKEN_EXPIRED']);
-//            } else if ($preException instanceof
-//                \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
-//                return response()->json(['error' => 'TOKEN_INVALID']);
-//            } else if ($preException instanceof
-//                \Tymon\JWTAuth\Exceptions\TokenBlacklistedException) {
-//                return response()->json(['error' => 'TOKEN_BLACKLISTED']);
-//            }
-//            if ($exception->getMessage() === 'Token not provided') {
-//                return response()->json(['error' => 'Token not provided']);
-//            }
-//        }
+        if ($exception instanceof UnauthorizedHttpException) {
+            $preException = $exception->getPrevious();
+            if ($preException instanceof
+                \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(ErrorMessage::getMessage(ErrorMessage::TOKEN_EXPIRED),401);
+            } else if ($preException instanceof
+                \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                // token 错误
+                return response()->json(ErrorMessage::getMessage(ErrorMessage::TOKEN_INVALID),401);
+            } else if ($preException instanceof
+                \Tymon\JWTAuth\Exceptions\TokenBlacklistedException) {
+                // token 列入黑名单
+                return response()->json(ErrorMessage::getMessage(ErrorMessage::TOKEN_BLACKLISTED),401);
+            }
+            if ($exception->getMessage() === 'Token not provided') {
+                return response()->json(ErrorMessage::getMessage(ErrorMessage::TOKEN_NOT_PROVIDED),401);
+            }
+        }
+
+        if ($exception instanceof
+            \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+            return response()->json(ErrorMessage::getMessage(ErrorMessage::TOKEN_CANT_REFRESHED),401);
+        }
+
         return parent::handle($exception);
     }
 }
